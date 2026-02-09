a1cdefc6979eaf79d892b4d385ac47fd03e8262d
Feone05 <fmnremoquillo@mymail.mapua.edu.ph>
2026-02-09 19:24:00 +0800

    mamaya naman

--- users/app.js
+++ users/app.js
@@ -1,5 +1,5 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-import { SUPABASE_CONFIG } from "./config.js";
+import { SUPABASE_CONFIG } from "../config.js";
 
 const supabase = createClient(SUPABASE_CONFIG.url, SUPABASE_CONFIG.anonKey);
 
